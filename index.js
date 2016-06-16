5c773262683c987fb0b0791f4ea8368f368639f4
alanev <e@alanev.ru>
2016-06-16 12:33:04 +0400

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var getPlaceholdURL = function( width, height, service ) {
         return 'https://placekitten.com/g/' + width + '/' + height;
     } else if ( service === 'dummyimage' ) {
         return 'https://dummyimage.com/' + width + 'x' + height;
-    } else {
+    } else if ( service === 'placeholdit' ) {
         return 'https://placehold.it/' + width + 'x' + height;
     } else {
         return service + width + 'x' + height;
